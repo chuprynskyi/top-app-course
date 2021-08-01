b637abe34c6ba9bf8114ebcb24b8b5b92a6b8997
Aleksandr <chuprynskyi_ak16@nuwm.edu.ua>
2021-08-01 16:29:06 +0300

    78 Sort + New Fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,9 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 		<Head>
 			<title>MyTop - наш лучший топ</title>
 			<link rel="icon" href="/favicon.ico" />
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" />
-			<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+			<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;700&display=swap" rel="stylesheet" />
 		</Head>
 		<Component {...pageProps} />
 	</>;
